601609521aa8c883523519909bc47ebccd242f9c
pedro <pedro.jose.bidoggia@hotmail.com>
2023-01-28 12:35:28 -0300

    handlers to return html files

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST resquest successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/home" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method not allowed", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "home page")
+}
+
+func main() {
+	filseServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", filseServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("home", homeHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
